2e798b928fa328967fbbdaf95102dbc218af1ddb
Luis Yudi <luisyudi1@gmail.com>
2023-09-06 12:39:20 -0300

    Adicao de alguns icones

--- Pages/home.php
+++ Pages/home.php
@@ -12,12 +12,140 @@
         <div id="logo">
             <img id="logoimage" src="Images/logo.png" alt="">
         </div>
-        <div id="menuoptions">
-            <a href="" class="option" id="about">About</a>
+        <div id="options-menu">
+            <a href="" class="about" id="about">About</a>
         </div>
     </header>
 
     <div id="main">
+        <div id="panel-box">
+            <div class="group-box">
+                <div class="option-box">
+                    <img src="../Images/Icons/muse.png" alt="" class="option-image">
+                    <input type="checkbox" id="muse" name="muse">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/bibi.png" alt="" class="option-image">
+                    <input type="checkbox" id="bibi" name="bibi">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/lilywhite.png" alt="" class="option-image">
+                    <input type="checkbox" id="lily white" name="lily white">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/printemps.png" alt="" class="option-image">
+                    <input type="checkbox" id="printemps" name="printemps">
+                </div>
+            </div>
+
+            <div class="group-box">
+                <div class="option-box">
+                    <img src="../Images/Icons/aqours.png" alt="" class="option-image">
+                    <input type="checkbox" id="aqours" name="aqours">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/azalea.png" alt="" class="option-image">
+                    <input type="checkbox" id="azalea" name="azalea">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/cyaron.png" alt="" class="option-image">
+                    <input type="checkbox" id="cyaron" name="cyaron">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/guiltykiss.png" alt="" class="option-image">
+                    <input type="checkbox" id="guilty kiss" name="guilty kiss">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/saintsnow.png" alt="" class="option-image">
+                    <input type="checkbox" id="saint snow" name="saint snow">
+                </div>
+            </div>
+
+            <div class="group-box">
+                <div class="option-box">
+                    <img src="../Images/Icons/nijigasaki.png" alt="" class="option-image">
+                    <input type="checkbox" id="nijigasaki" name="nijigasaki">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/azuna.png" alt="" class="option-image">
+                    <input type="checkbox" id="azuna" name="azuna">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/diverdiva.png" alt="" class="option-image">
+                    <input type="checkbox" id="diverdiva" name="diverdiva">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/qu4rtz.png" alt="" class="option-image">
+                    <input type="checkbox" id="qu4rtz" name="qu4rtz">
+                </div>
+
+                <div class="option-box">
+                    <img src="../Images/Icons/r3birth.png" alt="" class="option-image">
+                    <input type="checkbox" id="r3birth" name="r3birth">
+                </div>
+
+                <div class="option-box" id="nijigasaki-solo-box">
+                    <div class="nijigasaki-option" id="ayumu">
+                        <img src="../Images/Icons/ayumu.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+                    
+                    <div class="nijigasaki-option" id="kasumi">
+                        <img src="../Images/Icons/kasumi.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="shizuku">
+                        <img src="../Images/Icons/shizuku.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="karin">
+                        <img src="../Images/Icons/karin.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="ai">
+                        <img src="../Images/Icons/ai.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="kanata">
+                        <img src="../Images/Icons/kanata.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="setsuna">
+                        <img src="../Images/Icons/setsuna.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="emma">
+                        <img src="../Images/Icons/emma.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="rina">
+                        <img src="../Images/Icons/rina.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="shioriko">
+                        <img src="../Images/Icons/shioriko.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="mia">
+                        <img src="../Images/Icons/mia.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+
+                    <div class="nijigasaki-option" id="lanzhu">
+                        <img src="../Images/Icons/lanzhu.png" alt="" class="nijigasaki-solo-image">
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="button"><p class="button-text">Start</p></div>
     </div>
 
