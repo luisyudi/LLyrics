45b11dc1979592096fc8bf6ffef39bc47f9340ec
Luis Yudi <luisyudi1@gmail.com>
2023-09-08 00:08:34 -0300

    Modificaçoes na ediçao de musica

--- Pages/home.php
+++ Pages/home.php
@@ -20,11 +20,11 @@
     <div id="main">
         <div id="panel-box">
             <div class="group-box" id="group-box-muse">
-                <div class="option-box">
+                <div class="option-box option-box-main">
                     <img src="../Images/Icons/muse.png" alt="" class="option-image option-image-main">
                 </div>
-                <div class="group-settings-box">
-                    <p class="option-text">Subunits:</p>
+                <div class="group-settings-box group-settings-box-muse">
+                    <p class="option-text">Sub-units:</p>
                     <div class="subunit-box">
                         <div class="option-box option-box-subunit">
                             <img src="../Images/Icons/bibi.png" alt="" class="option-image">
@@ -55,97 +55,148 @@
             </div>
 
             <div class="group-box" id="group-box-aqours">
-                <div class="option-box">
+                <div class="option-box option-box-main">
                     <img src="../Images/Icons/aqours.png" alt="" class="option-image option-image-main">
                 </div>
+                <div class="group-settings-box">
+                    <p class="option-text">Sub-units:</p>
+                    <div class="subunit-box">
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/azalea.png" alt="" class="option-image">
+                        </div>
 
-                <div class="option-box">
-                    <img src="../Images/Icons/azalea.png" alt="" class="option-image">
-                </div>
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/cyaron.png" alt="" class="option-image">
+                        </div>
 
-                <div class="option-box">
-                    <img src="../Images/Icons/cyaron.png" alt="" class="option-image">
-                </div>
-
-                <div class="option-box">
-                    <img src="../Images/Icons/guiltykiss.png" alt="" class="option-image">
-                </div>
-
-                <div class="option-box">
-                    <img src="../Images/Icons/saintsnow.png" alt="" class="option-image">
-                </div>
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/guiltykiss.png" alt="" class="option-image">
+                        </div>
+                    </div>
+                    <div class="option-box">
+                        <img src="../Images/Icons/saintsnow.png" alt="" class="option-image">
+                    </div>
+                    <div class="subunit-box">
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-solo">Solo Songs</p>
+                        </div>
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-other">Other</p>
+                        </div>
+                    </div>
+                </div> 
             </div>
 
             <div class="group-box" id="group-box-nijigasaki">
-                <div class="option-box">
+                <div class="option-box option-box-main">
                     <img src="../Images/Icons/nijigasaki.png" alt="" class="option-image option-image-main">
                 </div>
+                <div class="group-settings-box">
+                    <p class="option-text">Sub-units:</p>
+                    <div class="subunit-box">
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/azuna.png" alt="" class="option-image">
+                        </div>
 
-                <div class="option-box">
-                    <img src="../Images/Icons/azuna.png" alt="" class="option-image">
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/diverdiva.png" alt="" class="option-image">
+                        </div>
+
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/qu4rtz.png" alt="" class="option-image">
+                        </div>
+                    </div>
+                    <div class="option-box">
+                        <img src="../Images/Icons/r3birth.png" alt="" class="option-image">
+                    </div>
+                    <div class="subunit-box">
+                        <div class="nijigasaki-option" id="ayumu">
+                            <img src="../Images/Icons/ayumu.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+                        
+                        <div class="nijigasaki-option" id="kasumi">
+                            <img src="../Images/Icons/kasumi.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="shizuku">
+                            <img src="../Images/Icons/shizuku.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="karin">
+                            <img src="../Images/Icons/karin.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="ai">
+                            <img src="../Images/Icons/ai.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="kanata">
+                            <img src="../Images/Icons/kanata.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="setsuna">
+                            <img src="../Images/Icons/setsuna.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="emma">
+                            <img src="../Images/Icons/emma.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="rina">
+                            <img src="../Images/Icons/rina.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="shioriko">
+                            <img src="../Images/Icons/shioriko.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="mia">
+                            <img src="../Images/Icons/mia.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+
+                        <div class="nijigasaki-option" id="lanzhu">
+                            <img src="../Images/Icons/lanzhu.png" alt="" class="nijigasaki-solo-image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="group-box" id="group-box-liella">
+                <div class="option-box option-box-main">
+                    <img src="../Images/Icons/liella.png" alt="" class="option-image option-image-main liella-image">
+                </div>
+                <div class="group-settings-box group-settings-box-liella">
+                    <p class="option-text">Sub-units:</p>
+                    <div class="subunit-box">
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/catchu.png" alt="" class="option-image">
+                        </div>
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/kaleidoscore.png" alt="" class="option-image">
+                        </div>
+                        <div class="option-box option-box-subunit">
+                            <img src="../Images/Icons/5yncri5e.png" alt="" class="option-image">
+                        </div>
+                    </div>
+                    <div class="subunit-box">
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-solo">Solo Songs</p>
+                        </div>
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-other">Other</p>
+                        </div>
+                    </div>
+                        
                 </div>
 
-                <div class="option-box">
-                    <img src="../Images/Icons/diverdiva.png" alt="" class="option-image">
-                </div>
-
-                <div class="option-box">
-                    <img src="../Images/Icons/qu4rtz.png" alt="" class="option-image">
-                </div>
-
-                <div class="option-box">
-                    <img src="../Images/Icons/r3birth.png" alt="" class="option-image">
-                </div>
-
-                <div class="option-box" id="nijigasaki-solo-box">
-                    <div class="nijigasaki-option" id="ayumu">
-                        <img src="../Images/Icons/ayumu.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-                    
-                    <div class="nijigasaki-option" id="kasumi">
-                        <img src="../Images/Icons/kasumi.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="shizuku">
-                        <img src="../Images/Icons/shizuku.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="karin">
-                        <img src="../Images/Icons/karin.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="ai">
-                        <img src="../Images/Icons/ai.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="kanata">
-                        <img src="../Images/Icons/kanata.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="setsuna">
-                        <img src="../Images/Icons/setsuna.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="emma">
-                        <img src="../Images/Icons/emma.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="rina">
-                        <img src="../Images/Icons/rina.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="shioriko">
-                        <img src="../Images/Icons/shioriko.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="mia">
-                        <img src="../Images/Icons/mia.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-
-                    <div class="nijigasaki-option" id="lanzhu">
-                        <img src="../Images/Icons/lanzhu.png" alt="" class="nijigasaki-solo-image">
-                    </div>
-                </div>
+            </div>
+                
+                
             </div>
         </div>
 
--- index.php
+++ index.php
@@ -17,3 +17,30 @@ switch ($url) {
 }
 
 ?>
+
+
+<!--
+            <div class="group-box" id="group-box-aqours">
+                <div class="option-box option-box-main">
+                    <img src="../Images/Icons/.png" alt="" class="option-image option-image-main">
+                </div>
+                <div class="group-settings-box">
+                    <p class="option-text">Subunits:</p>
+                    <div class="subunit-box">
+                        
+                    </div>
+                    <div class="option-box">
+                        <img src="../Images/Icons/saintsnow.png" alt="" class="option-image">
+                    </div>
+                    <div class="subunit-box">
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-solo">Solo Songs</p>
+                        </div>
+                        <div class="option-box solo-box">
+                            <input type="checkbox">
+                            <p class="option-other">Other</p>
+                        </div>
+                    </div>
+                </div> 
+            </div>
